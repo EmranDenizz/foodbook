53e839be6a560babf9a028c472261fae1e26d79a
Emran Deniz <63171566+EmranDenizz@users.noreply.github.com>
2022-12-01 01:11:53 +0300

    second update.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,30 @@ void main() {
 class MyApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFFE0F2F1),
+        appBar: AppBar(
+          backgroundColor: Color(0xFFE0F7FA),
+          title: Center(
+            child: Text(
+              'Yemek Listesi',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
+
+class YemekSayfasi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }

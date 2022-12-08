ab29ed7daa884899d08ff8d43c2406950c6b673a
Emran Deniz <63171566+EmranDenizz@users.noreply.github.com>
2022-12-08 03:50:39 +0300

    button added and activated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +17,7 @@ class MyApplication extends StatelessWidget {
           backgroundColor: Color(0xFFE0F7FA),
           title: Center(
             child: Text(
-              'Yemek Listesi',
+              'FOOD BOOK',
               style: TextStyle(color: Colors.black),
             ),
           ),
@@ -26,7 +28,26 @@ class MyApplication extends StatelessWidget {
   }
 }
 
-class YemekSayfasi extends StatelessWidget {
+class YemekSayfasi extends StatefulWidget {
+  const YemekSayfasi({Key? key}) : super(key: key);
+
+  @override
+  State<YemekSayfasi> createState() => _YemekSayfasiState();
+}
+
+class _YemekSayfasiState extends State<YemekSayfasi> {
+  int corbadeger = 1;
+  int yemekdeger = 1;
+  int tatlideger = 1;
+
+  void foodList() {
+    setState(() {
+      corbadeger = Random().nextInt(5) + 1;
+      yemekdeger = Random().nextInt(5) + 1;
+      tatlideger = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -40,10 +61,8 @@ class YemekSayfasi extends StatelessWidget {
                   style: TextButton.styleFrom(
                     backgroundColor: Colors.black,
                   ),
-                  onPressed: () {
-                    print('çorba tıklandı');
-                  },
-                  child: Image.asset('images/corba_1.jpg'),
+                  onPressed: foodList,
+                  child: Image.asset('images/corba_$corbadeger.jpg'),
                 ),
               ),
             ),
@@ -54,10 +73,8 @@ class YemekSayfasi extends StatelessWidget {
                 style: TextButton.styleFrom(
                   backgroundColor: Colors.black,
                 ),
-                onPressed: () {
-                  print('yemek 1 tıklandı');
-                },
-                child: Image.asset('images/yemek_1.jpg'),
+                onPressed: foodList,
+                child: Image.asset('images/yemek_$yemekdeger.jpg'),
               ),
             )),
             Expanded(
@@ -67,10 +84,8 @@ class YemekSayfasi extends StatelessWidget {
                   style: TextButton.styleFrom(
                     backgroundColor: Colors.black,
                   ),
-                  onPressed: () {
-                    print('tatlı 1 tıklandı');
-                  },
-                  child: Image.asset('images/tatli_1.jpg'),
+                  onPressed: foodList,
+                  child: Image.asset('images/tatli_$tatlideger.jpg'),
                 ),
               ),
             ),
